f76c6ffb20b1cb01b4e07a30cfd7885da2efd987
Ikromjon Ochilov <s288560@studenti.polito.it>
2021-07-25 23:32:25 +0200

    fixed errors

--- javascript/main.js
+++ javascript/main.js
@@ -31,27 +31,47 @@ SoundCloudAPI.renderTracks = function(){
   var card = document.createElement('div');
   card.classList.add('card');
 
-  var searchResults = document.querySelector('.js-search-results');
-  searchResults.appendChild(card);
 
-  var image = document.createElement('div');
-  image.classList.add('image');
+  var imageDiv = document.createElement('div');
+  imageDiv.classList.add('image');
+  
+  var image_img = document.createElement('img');
+  image_img.classList.add('image_img');
+  image_img.src = 'http://www.placekitten.com/290/290';
+
+  imageDiv.appendChild(image_img);
+
+  //content
   var content = document.createElement('div');
   content.classList.add('content');
+
   var header = document.createElement('div');
   header.classList.add('header');
-  var ui_button = document.createElement('div');
-  ui_button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
-  var add_icon = document.createElement('div');
-  add_icon.classList.add('add', 'icon');
+  header.innerHTML = '<a href="#" target="_blank">"Science Vs. Romance"</a>'
 
-  card.appendChild(image);
-  card.appendChild(content);
-  card.appendChild(ui_button);
+  //button
+  var button = document.createElement('div');
+  button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+  
+  var icon = document.createElement('div');
+  icon.classList.add('add', 'icon');
+
+  var buttonText = document.createElement('span');
+  buttonText.innerHTML = 'Add to Playlist';
+
+  //appendChild
+  /// content
   content.appendChild(header);
-  ui_button.appendChild(add_icon);
+  /// button
+  button.appendChild(add_icon);
+  button.appendChild(buttonText);
 
+  card.appendChild(imageDiv);
+  card.appendChild(content);
+  card.appendChild(button);
 
+  var searchResults = document.querySelector('.js-search-results');
+  searchResults.appendChild(card);
 }
 
 SoundCloudAPI.renderTracks();
